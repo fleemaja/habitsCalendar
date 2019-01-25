0702fc6b94af2d960a9d3b491b4140237e063d26
Drew Fleeman <drew.fleeman@gmail.com>
2019-01-25 17:24:01 -0600

    add edit and add buttons to habits header navigation

--- screens/HabitsScreen.js
+++ screens/HabitsScreen.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, Button, FlatList } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, Button, FlatList } from 'react-native';
 
 export default class HabitsScreen extends React.Component {
 
-  static navigationOptions = {
-    title: 'Habits',
+  static navigationOptions = ({ navigation }) => {
+    const { params = {} } = navigation.state;
+    return {
+      title: 'Habits',
+      headerLeft: (
+        <Button
+          onPress={() => params.toggleEdit()}
+          title="Edit"
+        />
+      ),
+      headerRight: (
+        <Button
+          onPress={() => navigation.navigate('AddHabitForm', {
+            addHabit: params.addHabit
+          })}
+          title="Add"
+        />
+      ),
+    }
   };
 
   constructor(props) {
     super(props)
     this.state = {
+      editMode: false,
       data: [
         {
           key: "Floss",
@@ -23,10 +41,49 @@ export default class HabitsScreen extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.props.navigation.setParams({
+      addHabit: this.addHabit.bind(this),
+      toggleEdit: this.toggleEdit.bind(this)
+    });
+  }
+
   addHabit(habit) {
     this.setState(prevState => (
       { data: [habit, ...prevState.data] }
     ))
+  }
+
+  toggleEdit() {
+    this.setState(prevState => (
+      { editMode: !prevState.editMode }
+    ))
+  }
+
+  _deleteItem(item) {
+    this.setState(prevState => {
+      const data = prevState.data.filter((d) => d.key != item.key);
+      return { data }
+    })
+  }
+
+  _renderHabitRow(item) {
+    return (
+      <View>
+        <TouchableOpacity style={styles.row}
+          onPress={() => this.props.navigation.navigate('HabitDetail', {item})}>
+          <Text style={styles.label}>{item.key}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+          {
+            (this.state.editMode) &&
+            (<Button
+              onPress={() => this._deleteItem(item)}
+              title="DELETE"
+              color="#ff0000" />)
+          }
+        </TouchableOpacity>
+      </View>
+    )
   }
 
   render() {
@@ -34,19 +91,23 @@ export default class HabitsScreen extends React.Component {
       <ScrollView style={styles.container}>
         <FlatList
           data={this.state.data}
-          renderItem={({item}) =>
-            <Text
-              onPress={() => this.props.navigation.navigate('HabitDetail', {item})}>
-              {item.key} - {item.description}
-            </Text>
-          }
+          extraData={this.state}
+          renderItem={({item}) => this._renderHabitRow(item)}
           />
-        <Button
-          title="Add New Habit"
-          onPress={() => this.props.navigation.navigate('AddHabitForm', {
-            addHabit: this.addHabit.bind(this)
-          })}
-        />
+        {
+          (this.state.data.length === 0) &&
+            (
+              <View>
+                <Text>No habits added yet!</Text>
+                <Button
+                  title="Add New Habit"
+                  onPress={() => this.props.navigation.navigate('AddHabitForm', {
+                    addHabit: this.addHabit.bind(this)
+                  })}
+                />
+              </View>
+          )
+        }
       </ScrollView>
     );
   }
@@ -58,4 +119,17 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  row: {
+    margin: 5,
+    padding: 5,
+    borderWidth: 1,
+    backgroundColor: '#fff'
+  },
+  label: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  description: {
+    fontSize: 16
+  }
 });
