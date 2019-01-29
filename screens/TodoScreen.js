b56a941094b58add240b6467156486ca7e3d8f05
Drew Fleeman <drew.fleeman@gmail.com>
2019-01-29 16:56:33 -0600

    add and complete todo functionality

--- screens/TodoScreen.js
+++ screens/TodoScreen.js
@@ -20,10 +20,36 @@ export default class TodoScreen extends React.Component {
     this.state = {
       text: '',
       data: [
-        { key: 'water the venus fly trap plants' },
-        { key: "brush my hamster's teeth" },
+        { key: 'water the venus fly trap plants', completed: false },
+        { key: "brush my hamster's teeth", completed: false },
       ]
     };
+  }
+
+  addTodo() {
+    const newTodo = { key: this.state.text, completed: false }
+    this.setState(prevState => ({
+      data: [...prevState.data, newTodo],
+      text: ''
+    }))
+  }
+
+  _toggleCompleted(item) {
+    this.setState((prevState) => {
+      const toggledData = prevState.data.map((d) =>
+        (d.key === item.key) ? { key: d.key, completed: !d.completed } : d
+      )
+      return { data: toggledData }
+    })
+  }
+
+  _renderTodo(item) {
+    const textStyle = item.completed ? [styles.label, styles.completed] : [styles.label];
+    return (
+      <TouchableOpacity style={styles.row} onPress={() => this._toggleCompleted(item)}>
+        <Text style={textStyle}>{item.key}</Text>
+      </TouchableOpacity>
+    )
   }
 
   render() {
@@ -31,12 +57,14 @@ export default class TodoScreen extends React.Component {
       <View style={styles.container}>
         <FlatList
           data={this.state.data}
-          renderItem={({item}) => <Text>{item.key}</Text>}
+          extraData={this.state}
+          renderItem={({item}) => this._renderTodo(item)}
         />
         <TextInput
-          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          style={styles.todoInput}
           onChangeText={(text) => this.setState({text})}
           value={this.state.text}
+          onSubmitEditing={() => this.addTodo()}
           placeholder="New Todo"
           maxLength={280}
         />
@@ -50,5 +78,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    margin: 10,
+  },
+  todoInput: {
+    height: 40,
+    margin: 20,
+    padding: 10,
+    borderColor: 'blue',
+    borderRadius: 10,
+    borderWidth: 2
+  },
+  row: {
+    margin: 5,
+    padding: 5,
+    borderWidth: 1,
+    backgroundColor: '#fff'
+  },
+  completed: {
+    textDecorationLine: 'line-through',
+    textDecorationStyle: 'solid'
+  },
+  label: {
+    fontSize: 24,
   },
 });
