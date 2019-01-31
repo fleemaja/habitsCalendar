dbbd8a204921080bb6924fc0b8801f4d30e79e9f
Drew Fleeman <drew.fleeman@gmail.com>
2019-01-31 15:14:56 -0600

    use date api to get a list of all days of the year

--- screens/MoodScreen.js
+++ screens/MoodScreen.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import { ScrollView, StyleSheet, Text, FlatList } from 'react-native';
 
 export default class MoodScreen extends React.Component {
   static navigationOptions = {
-    title: 'Mood Calendar',
+    title: 'Mood Calendar'
   };
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: this.getDays()
+    }
+  }
+
+  getDayObject(d) {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+                  'Thursday', 'Friday', 'Saturday'],
+          months = ["January", "February", "March",
+                    "April", "May", "June", "July",
+                    "August", "September", "October",
+                    "November", "December"];
+          dayIdx = d.getDay(),
+          dayOfTheWeek = days[dayIdx],
+          monthName = months[d.getMonth()],
+          dayOfMonth = d.getDate(),
+          dayYear = d.getFullYear(),
+          dayString = `${dayOfTheWeek} ${monthName} ${dayOfMonth}, ${dayYear}`;
+    return { "key": dayString };
+  }
+
+  getDays() {
+    const date = new Date(),
+          year = date.getFullYear(),
+          month = date.getMonth();
+
+    let data = [];
+    for (let i = 1; i <= 365; i++) {
+      const d = new Date(year, month, i);
+      const dayObject = this.getDayObject(d);
+      data.push(dayObject);
+    }
+
+    return data;
+  }
 
   render() {
     return (
       <ScrollView style={styles.container}>
-        <Text>
-          This will be a mood calendar. The calendar will be editable and notes
-          will be viewable and addable.
-        </Text>
+        <FlatList
+          data={this.state.data}
+          renderItem={({item}) => <Text>{item.key}</Text>} />
       </ScrollView>
     );
   }
