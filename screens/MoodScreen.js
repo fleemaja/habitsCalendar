d29f405794e01d2bf3e9f34be1e93490ae32ab17
Drew Fleeman <drew.fleeman@gmail.com>
2019-02-01 15:35:13 -0600

    use sectionlist to label and render month calendar displays

--- screens/MoodScreen.js
+++ screens/MoodScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, FlatList } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, SectionList } from 'react-native';
 
 export default class MoodScreen extends React.Component {
   static navigationOptions = {
@@ -19,14 +19,15 @@ export default class MoodScreen extends React.Component {
           months = ["January", "February", "March",
                     "April", "May", "June", "July",
                     "August", "September", "October",
-                    "November", "December"];
+                    "November", "December"],
           dayIdx = d.getDay(),
           dayOfTheWeek = days[dayIdx],
-          monthName = months[d.getMonth()],
+          monthIdx = d.getMonth(),
+          monthName = months[monthIdx],
           dayOfMonth = d.getDate(),
           dayYear = d.getFullYear(),
-          dayString = `${dayOfTheWeek} ${monthName} ${dayOfMonth}, ${dayYear}`;
-    return { "key": dayString };
+          nameString = `${dayOfTheWeek} ${monthName} ${dayOfMonth}, ${dayYear}`;
+    return { "month": monthName, "year": dayYear, dayOfTheWeek, dayOfMonth, nameString };
   }
 
   getDays() {
@@ -38,18 +39,51 @@ export default class MoodScreen extends React.Component {
     for (let i = 1; i <= 365; i++) {
       const d = new Date(year, month, i);
       const dayObject = this.getDayObject(d);
-      data.push(dayObject);
+      const section = `${dayObject.month} ${dayObject.year}`;
+
+      let sectionFound = false;
+      data.forEach((obj, idx) => {
+        if (obj.title === section) {
+          sectionFound = true;
+          data[idx]['data'][0]["days"].push(dayObject);
+        }
+      })
+      if (!sectionFound) {
+        const newSection = { "title": section, data: [{ "days" : [dayObject] }] }
+        data.push(newSection);
+      }
     }
 
     return data;
   }
 
+  renderMonth(index, item) {
+    const days = item.days;
+    const saturdays = days.filter((d) => d.dayOfTheWeek === "Saturday");
+    const sundays = days.filter((d) => d.dayOfTheWeek === "Sunday");
+    return (
+      <View key={index} style={styles.monthContainer}>
+        <View style={styles.weekdayColumnContainer}>
+          { sundays.map((s) => <Text key={s.dayOfMonth} styles={styles.sundays}>{s.dayOfMonth}</Text>) }
+        </View>
+        <View style={styles.weekdayColumnContainer}>
+          { saturdays.map((s) => <Text key={s.dayOfMonth} styles={styles.saturdays}>{s.dayOfMonth}</Text>) }
+        </View>
+      </View>
+    )
+  }
+
   render() {
     return (
       <ScrollView style={styles.container}>
-        <FlatList
-          data={this.state.data}
-          renderItem={({item}) => <Text>{item.key}</Text>} />
+        <SectionList
+          renderItem={({item, index, section}) => this.renderMonth(index, item)}
+          renderSectionHeader={({section: {title}}) => (
+            <Text style={styles.sectionHeader}>{title}</Text>
+          )}
+          sections={this.state.data}
+          keyExtractor={(item, index) => item + index}
+        />
       </ScrollView>
     );
   }
@@ -61,4 +95,18 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     backgroundColor: '#fff',
   },
+  sectionHeader: {
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  monthContainer: {
+    flexDirection: 'row',
+  },
+  weekdayColumnContainer: {
+    flexDirection: 'column'
+  },
+  sundays: {
+  },
+  saturdays: {
+  }
 });
